fa23c40a5539ffb5935ef68f2c3517fde493de01
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2025-04-18 20:06:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ from plugins.web_support import web_server
 from dotenv import load_dotenv
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-PORT = "8300" 
+PORT = "8350" 
 # Get logging configurations
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
